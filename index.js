91af0c0474898ed4bf1f81b435626acb3a044727
Laila Nabil <laila.nabil.mustafa@gmail.com>
2024-01-19 22:44:26 +0200

    works put when sending to server not server/clickup

--- index.js
+++ index.js
@@ -1,62 +1,67 @@
 const express = require('express');
 const httpProxy = require('http-proxy');
-const axios = require('axios'); // Replace 'request' with 'axios'
+const rTracer = require('cls-rtracer')
 
 const app = express();
-const CLICKUP_API_BASE_URL = 'https://api.clickup.com/api/v2'; // ClickUp API base URL
+const CLICKUP_API_BASE_URL = 'https://api.clickup.com/api/v2';
 
-// Create a proxy server with custom application logic
 const proxy = httpProxy.createProxyServer({});
+app.use(rTracer.expressMiddleware())
 
+
+// Add event listeners to the proxy instance
+proxy.on('proxyReq', (proxyReq, req, res, options) => {
+
+  console.log('Proxy Request:');
+  console.log(`- Method: ${req.method}`);
+  console.log(`- Path: ${req.originalUrl}`);
+  console.log(`- Headers: ${JSON.stringify(proxyReq.getHeaders())}`);
+  console.log(`- Body: ${JSON.stringify(proxyReq.body)}`);
+});
+
+proxy.on('proxyRes', (proxyRes, req, res) => {
+  // console.log('Proxy Response:');
+  // console.log(`- Status Code: ${proxyRes.statusCode}`);
+  // console.log(`- Headers: ${JSON.stringify(proxyRes.headers)}`);
+});
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE', 'OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
-// Handle different ClickUp API endpoints
-app.all('/clickup/*', async (req, res) => {
-  console.log(`req.params[0] ${req.params[0]}`);
-  console.log(`req.headers ${req.headers}`);
-  console.log(`req.method ${req.method}`);
-  console.log(`req.headers Authorization ${req.headers["Authorization"]}`);
+app.all('/*', async (req, res) => {
+  // console.log(`Incoming Request:`);
+  // console.log(`- Path: ${req.originalUrl}`);
+  // console.log(`- Method: ${req.method}`);
+  // console.log(`- Headers: ${JSON.stringify(req.headers)}`);
+  // console.log(`- Query Params: ${JSON.stringify(req.query)}`);
+  // console.log(`- Body: ${JSON.stringify(req.body)}`);
 
-  const apiUrl = `${CLICKUP_API_BASE_URL}/${req.params[0]}`; // Combine base URL with requested endpoint
-  console.log(`apiUrl ${apiUrl}`);
-  // Extract relevant details from the incoming request
+  const apiUrl = `${CLICKUP_API_BASE_URL}`;
   const requestOptions = {
     method: req.method,
     url: apiUrl,
-    // headers: {
-    //   'Content-Type': 'application/json',
-    //   // Add any additional headers required for the ClickUp API
-    //   // Authorization: 'Bearer YOUR_CLICKUP_API_KEY',
-    // },
-    headers: req.headers
+    headers: req.headers,
   };
 
-  // For GET requests, include query parameters
   if (req.method === 'GET') {
     requestOptions.params = req.query;
   }
 
-  // For POST, PUT, and DELETE requests, include the request body
   if (['POST', 'PUT', 'DELETE'].includes(req.method)) {
     requestOptions.data = req.body;
   }
-  console.log(`requestOptions.data ${requestOptions.data}`);
-  console.log(`requestOptions.params ${requestOptions.params}`);
-  console.log(`requestOptions.url ${requestOptions.url}`);
-  console.log(`requestOptions.method ${requestOptions.method}`);
-  console.log(`requestOptions.url ${requestOptions.url}`);
-  
-  // Forward the request to the target host
+
+  // console.log(`Forwarding Request to ${apiUrl}`);
+  // console.log(`- Method: ${requestOptions.method}`);
+  // console.log(`- Headers: ${JSON.stringify(requestOptions.headers)}`);
+  // console.log(`- Query Params: ${JSON.stringify(requestOptions.params)}`);
+  // console.log(`- Body: ${JSON.stringify(requestOptions.data)}`);
   proxy.web(req, res, { target: apiUrl, changeOrigin: true }, (error) => {
-    // Handle any errors from the proxy or the target server
     res.status(500).send('An error occurred: ' + error.message);
   });
-
 });
 
 const PORT = process.env.PORT || 5000;
